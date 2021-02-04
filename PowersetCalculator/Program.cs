c281275dda43a0e5b1a0dec6468bca2f0f444873
Valentin <valentin.aj.roy@gmail.com>
2021-02-04 23:10:57 +0000

    Cleanup Program.cs

--- PowersetCalculator/Program.cs
+++ PowersetCalculator/Program.cs
@@ -9,9 +9,6 @@ namespace PowersetCalculator
         {
             Console.WriteLine("Enter your set (integers separated by space) e.g. 1 2 3 4 5:");
 
-            //var set = new[] {1, 2, 3};
-            //var list = new List<int>();
-            //var set = list.ToArray(); 
             var userinput = Console.ReadLine()?.Split(' ');
             var set = (userinput ?? throw new ArgumentNullException()).Select(int.Parse).ToArray();
             
