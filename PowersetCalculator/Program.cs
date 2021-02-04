f14183a71013f7c991149142d59053051d5c09c6
Valentin <valentin.aj.roy@gmail.com>
2021-02-04 13:24:16 +0000

    Update Program.cs with User input

--- PowersetCalculator/Program.cs
+++ PowersetCalculator/Program.cs
@@ -7,13 +7,16 @@ namespace PowersetCalculator
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Enter your set (integers separated by space) e.g. 1 2 3 4 5:");
-            
-            //var userinput = Console.ReadLine()?.Split(' ');
-            //var set = (userinput ?? throw new ArgumentNullException()).Select(int.Parse).ToArray();
-            var set = new[] {1, 2, 3};
+            Console.WriteLine("Enter your set (integers separated by space) e.g. 1 2 3 4 5:");
 
-            //var subsets = SetsUtilities.Combinations(set, 0); 
+            //var set = new[] {1, 2, 3};
+            //var list = new List<int>();
+            //var set = list.ToArray(); 
+            var userinput = Console.ReadLine()?.Split(' ');
+            var set = (userinput ?? throw new ArgumentNullException()).Select(int.Parse).ToArray();
+            
+            Console.WriteLine();
+            
             var subsets = SetsUtilities.Powerset(set);
 
             foreach (var combination in subsets)
